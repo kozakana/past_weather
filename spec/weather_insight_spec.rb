21395ec168fd17ae4ba20325ca52b8b79b5b857d
mori-goki <mori-goki@kayac.com>
2015-05-29 09:27:46 +0900

    Add test

--- spec/weather_insight_spec.rb
+++ spec/weather_insight_spec.rb
@@ -5,7 +5,11 @@ describe WeatherInsight do
     expect(WeatherInsight::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'can acquire data of January' do
+    month_data = WeatherInsight.month_data :tokyo, 1
+    expect(month_data.length).to eq 31
+    month_data.each do |day|
+      expect(day).not_to be nil
+    end
   end
 end
